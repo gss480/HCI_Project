6ee522e99764f3faf89639f189ef95c194a43f00
Ruud van Bakel <ruudvanbakel@yahoo.co.uk>
2019-06-17 11:30:48 +0200

    Made a basic setup for the site

--- HCI_site/Source_Code/javascript/index.js
+++ HCI_site/Source_Code/javascript/index.js
@@ -1,6 +1,16 @@
 // JavaScript source code
 $(document).ready(function () {
+    var hidden = false;
+
     $("button").click(function () {
-        $("#Testrow").hide();
+
+        if (!hidden) {
+            $("#Testrow").hide();
+            hidden = true;
+        }
+        else {
+            $("#Testrow").show();
+            hidden = false;
+        }
     });
 });
